a9471247f2553a3fdfebf363c907a9bbff8f1620
tinroy <thesurvivor0701@gmail.com>
2025-12-29 18:56:37 +0530

    Update schema.sql

--- dannys-diner/schema.sql
+++ dannys-diner/schema.sql
@@ -1,1 +1,22 @@
+-- creating database
+create database dannys_diner;
+-- using database
+use dannys_diner;
 
+-- create tables
+create table members
+	(customer_id char(1) primary key,
+    join_date datetime);
+    
+create table menu
+	(product_id int primary key auto_increment,
+    product_name varchar(100) not null,
+    price int not null);
+
+create table sales
+	(customer_id char(1) not null,
+    order_date datetime not null,
+    order_id int primary key auto_increment,
+    product_id int not null,
+    foreign key (customer_id) references members(customer_id),
+    foreign key (product_id) references menu (product_id));
